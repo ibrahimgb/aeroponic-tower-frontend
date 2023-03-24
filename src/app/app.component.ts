4109a41b9856b94d69d6df4c07fb71ce05dae0a4
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-03-24 13:22:35 +0100

    style customization and adding side nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular101';
+  isDarkTheme = false;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,13 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { HomeModule} from "./home/home.module"
 import { MatMenuModule} from '@angular/material/menu';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
@@ -25,7 +32,12 @@ import { MatMenuModule} from '@angular/material/menu';
     MaterialModule,
     FontAwesomeModule,
     HomeModule,
-    MatMenuModule
+    MatMenuModule,
+    MatFormFieldModule,
+    FormsModule, ReactiveFormsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
