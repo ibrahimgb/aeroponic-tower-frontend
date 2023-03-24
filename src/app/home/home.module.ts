4fd9ddf4974a54ec0d76cd698686d7d4199dc76a
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-03-24 13:20:35 +0100

    add navbar

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,19 +4,32 @@ import { HomeComponent } from './home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatMenuModule} from '@angular/material/menu';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SidenavComponent } from './sidenav/sidenav.component'
-
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { MatRippleModule} from '@angular/material/core';
+import { SensersReadingsComponent } from './sensers-readings/sensers-readings.component';
+import { MaterialModule } from "../material.module";
+import { ReadingComponent } from './sensers-readings/reading/reading.component';
+import { ReadingDirective } from './sensers-readings/reading.directive';
+import { LineChartComponent } from './line-chart/line-chart.component'
 
 @NgModule({
   declarations: [
     HomeComponent,
     NavbarComponent,
-    SidenavComponent
+    SidenavComponent,
+    SensersReadingsComponent,
+    ReadingComponent,
+    ReadingDirective,
+    LineChartComponent
   ],
   imports: [
     CommonModule,
     MatMenuModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatSlideToggleModule,
+    MatRippleModule,
+    MaterialModule
   ],
   exports: [
     HomeComponent
