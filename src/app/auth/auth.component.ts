e5f659c1c882f2d9cde89ae502fceb9381a19ad7
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-04-10 23:39:14 +0100

    login ui

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from './auth.service';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -7,4 +9,64 @@ import { Component } from '@angular/core';
 })
 export class AuthComponent {
   signIn: boolean = true;
+
+  signUpForm!: FormGroup;
+  logInForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private _snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit() {
+    this.logInForm = this.fb.group({
+      email: '',
+      password: '',
+    });
+
+    this.signUpForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    });
+  }
+
+  login() {
+    const logIn = this.logInForm.getRawValue();
+
+    if (!logIn.email) {
+      this.openSnackBar('Please provide a valid email');
+    } else {
+      //console.log(logIn);
+      this.authService.logIn(logIn);
+    }
+  }
+
+  singUp() {
+    // this.signIn = false;
+
+    const singUp = this.signUpForm.getRawValue();
+    if (!singUp.firstName) {
+      this.openSnackBar('Please provide your firsName');
+    } else if (!singUp.lastName) {
+      this.openSnackBar('Please provide your lastName');
+    } else if (!singUp.email) {
+      this.openSnackBar('Please provide a valid email');
+    } else if (singUp.password != singUp.passwordConfirm) {
+      this.openSnackBar('Password not match, Try again');
+      return;
+    } else if (singUp.password.length < 1) {
+      this.openSnackBar('Password is Weak, Try more complex password');
+    } else {
+      delete singUp.passwordConfirm;
+      //console.log(singUp);
+      this.authService.singUp(singUp);
+    }
+  }
+  openSnackBar(message: string) {
+    this._snackBar.open(message, 'Ok', {});
+  }
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthComponent } from './auth.component';
 import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [AuthComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [AuthComponent],
   providers: [AuthService],
 })
