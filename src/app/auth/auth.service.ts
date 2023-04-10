3bc0fbae35cec8a84e90241e8e6806126804bff6
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-04-10 23:39:01 +0100

    login/signup logique storing token in localStorage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  singUp(data: object) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    const requestOptions = { headers: headers };
+
+    return this.http
+      .post('http://192.168.1.14:3000/auth/signup', data, requestOptions)
+      .subscribe((token: any) => {
+        this.setSession(token);
+      });
+  }
+
+  logIn(data: object) {
+    return this.http
+      .post('http://192.168.1.14:3000/auth/signin', data)
+      .subscribe((token: any) => {
+        this.setSession(token);
+      });
+  }
+
+  private setSession(authResult: any) {
+    localStorage.setItem('access_token', authResult.access_token);
+  }
 }
